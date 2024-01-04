b8e2d80c0932bcb04889206d89898c678857a3f3
Iván Moreno <ivanmorenogf@hotmail.com>
2024-01-04 11:54:29 +0100

    test: use ternary operator

--- FirstRubyProject/public/calculator.rb
+++ FirstRubyProject/public/calculator.rb
@@ -2,7 +2,7 @@
 
 class Calculator
   def self.add(one, two)
-    one + two
+    one == 1 ? two : one + two 
   end
 
   def add_from_instance(one, two)
--- FirstRubyProject/test/calculator_tests_spec.rb
+++ FirstRubyProject/test/calculator_tests_spec.rb
@@ -5,8 +5,11 @@ require_relative '../public/calculator'
 RSpec.describe 'MyRspecTest' do
   context 'when condition' do
     it '2 + 1' do
-      @sut = Calculator.new()
-      expect(@sut.add_from_instance(2, 1)).to be(3)
+      sut = Calculator.new()
+      expect(sut.add_from_instance(2, 1)).to be(3)
+    end
+    it '1 as first input returns second input' do
+      expect(Calculator.add(1, 6)).to be(6)
     end
     it '2 + 5 fails' do
       expect(Calculator.add(2,5)).to be(4)

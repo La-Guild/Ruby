b562425cbc4f67c7a7c5b0541ef4be7c3f06207f
Xiphereal <lozanojalex@gmail.com>
2024-01-06 11:25:50 +0100

    docs: add stage 2 requirements

--- FirstRubyProject/test/my_tests_spec.rb
+++ FirstRubyProject/test/my_tests_spec.rb
@@ -7,10 +7,13 @@ require_relative '../public/fizz_buzz'
 # Devolver Buzz si es múltiplo de 5
 # Devolver FizzBuzz si es múltiplo de 3 y de 5
 
+# Si la palabra contiene un 3 se añadie un Fizz adicional al final
+# Si la palabra contiene un 5 se añadie un Buzz adicional al final
+
 RSpec.describe 'MyRspecTest' do
 
   context 'when condition' do
-    it 'succeeds' do
+    it 'stage 1' do
       expect(FizzBuzz.speak(1)).to be(1)
       expect(FizzBuzz.speak(3)).to be("Fizz")
       expect(FizzBuzz.speak(5)).to be("Buzz")

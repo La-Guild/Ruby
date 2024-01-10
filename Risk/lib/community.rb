cbd11c23b37717ff06a535d2b6f0b270dea01704
Xiphereal <lozanojalex@gmail.com>
2024-01-10 20:23:02 +0100

    refactor: coin Community

--- Risk/lib/community.rb
+++ Risk/lib/community.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Aasfadsfa
+class Community
   attr_reader :units
 
   def initialize
--- Risk/spec/dasfas_spec.rb
+++ Risk/spec/dasfas_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative '../lib/aasfadsfa'
+require_relative '../lib/community'
 
 describe 'Dasfas' do
   context 'when condition' do
     it 'succeeds' do
-      expect(Aasfadsfa.new().units.length).to be(0)
+      expect(Community.new.units.length).to be(0)
     end
   end
 end

8bad11710500eab8b0d551c923e9491c2f1c2998
Xiphereal <lozanojalex@gmail.com>
2024-01-14 12:24:29 +0100

    test: minor coining of names to specs

--- Risk/spec/risk_spec.rb
+++ Risk/spec/risk_spec.rb
@@ -6,7 +6,17 @@ require_relative '../lib/risk'
 
 describe 'Risk_spec' do
   context 'Risk' do
-    it 'should ' do
+    it 'asdfljkf units to players' do
+      sut = Risk.new
+      doc = Player.new
+
+      sut.sit(doc)
+      sut.start
+
+      expect(doc.recruited_units).to be(15)
+    end
+
+    it 'sdfklgsdfgl communities to players' do
       sut = Risk.new
       doc = Player.new
 

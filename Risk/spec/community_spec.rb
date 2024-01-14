94773cb95df3553c28608aabcb2c18814c2879f4
Xiphereal <lozanojalex@gmail.com>
2024-01-14 11:16:21 +0100

    test: coin names to tests

--- Risk/spec/community_spec.rb
+++ Risk/spec/community_spec.rb
@@ -3,22 +3,22 @@
 require 'rspec'
 require_relative '../lib/community'
 
-describe 'Dasfas' do
-  context 'when condition' do
-    it 'succeeds' do
+describe 'Community' do
+  context 'when being occupied' do
+    it 'is empty by default' do
       expect(Community.new.units).to be(0)
     end
-    it 'asdfasdf' do
+    it 'can be occupied by one unit' do
       sut = Community.new
       sut.occupy_by(1)
       expect(sut.units).to be(1)
     end
-    it 'should ' do
+    it 'can be occupied by several units' do
       sut = Community.new
       sut.occupy_by(2)
       expect(sut.units).to be(2)
     end
-    it 'should asdfasdf' do
+    it 'can be left by units' do
       sut = Community.new
       sut.occupy_by(2)
       sut.leave(2)

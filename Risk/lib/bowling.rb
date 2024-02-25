e2010fab5a36958054b339971451d5a74160716b
Xiphereal <lozanojalex@gmail.com>
2024-02-25 17:16:55 +0100

    refactor: frames are finished after 2nd roll

--- Risk/lib/bowling.rb
+++ Risk/lib/bowling.rb
@@ -11,10 +11,14 @@ class Bowling
         @frames.push(Frame.new)
         @frames.push(Frame.new)
         @frames.push(Frame.new)
+
+        @current_frame = 0
     end
 
     def roll(pins)
-        @frames[0].roll(pins)
+        @frames[@current_frame].roll(pins)
+
+        @current_frame += 1 if @frames[@current_frame].is_finished?
     end
 
     def score
@@ -32,6 +36,7 @@ class Frame
     end
 
     def roll(pins)
+        raise 'frames currently only support 2 rolls' if is_finished?
         @score += pins
         @rolls += 1
     end

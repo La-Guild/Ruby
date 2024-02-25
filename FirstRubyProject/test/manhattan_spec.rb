6284566c157ed972974b1dc80452a9720b328989
Xiphereal <lozanojalex@gmail.com>
2024-02-25 18:01:55 +0100

    refactor: add Point class

--- FirstRubyProject/test/manhattan_spec.rb
+++ FirstRubyProject/test/manhattan_spec.rb
@@ -2,12 +2,19 @@ require 'rspec'
 
 describe 'Manhattan distance' do
   it 'asdfaf' do
-    expect(Asdfasfd.manhattan_distance(1, 1)).to be(0)
+    expect(Asdfasfd.manhattan_distance(Point.new(1, 1), Point.new(1, 1))).to be(0)
   end
 end
 
 class Asdfasfd
   def self.manhattan_distance(point1, point2)
     0
+  end
+end
+
+class Point
+  def initialize(x, y)
+    @x = x
+    @y = y
   end
 end

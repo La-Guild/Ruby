5782a2c6c3ee3375600cbb2a47deec84a225d708
Xiphereal <lozanojalex@gmail.com>
2024-02-25 17:59:23 +0100

    feat: stablish public API

---
+++ FirstRubyProject/test/manhattan_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+describe 'Manhattan distance' do
+  it 'asdfaf' do
+    expect(Asdfasfd.manhattan_distance(1, 1)).to be(0)
+  end
+end
+
+class Asdfasfd
+  def self.manhattan_distance(point1, point2)
+    0
+  end
+end

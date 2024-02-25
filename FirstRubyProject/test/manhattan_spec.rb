e1655123b81d9079342cf5f467c757f23b93dbbf
Xiphereal <lozanojalex@gmail.com>
2024-02-25 18:12:23 +0100

    feat: add calculation of manhattan distance

--- FirstRubyProject/test/manhattan_spec.rb
+++ FirstRubyProject/test/manhattan_spec.rb
@@ -1,20 +1,30 @@
 require 'rspec'
 
 describe 'Manhattan distance' do
-  it 'asdfaf' do
+  it '(1,1) to (1,1) == 0' do
     expect(Asdfasfd.manhattan_distance(Point.new(1, 1), Point.new(1, 1))).to be(0)
+  end
+
+  it '(1,1) to (1,2) == 1' do
+    expect(Asdfasfd.manhattan_distance(Point.new(1, 1), Point.new(1, 2))).to be(1)
   end
 end
 
 class Asdfasfd
   def self.manhattan_distance(point1, point2)
-    0
+    point1.distance_to(point2)
   end
 end
 
 class Point
+  attr_reader :x, :y
+
   def initialize(x, y)
     @x = x
     @y = y
+  end
+
+  def distance_to(another)
+    (@x.abs - another.x.abs + @y.abs - another.y.abs).abs
   end
 end

96f2c0322e51d0d586e372e31658cef89a460e0c
Iván Moreno <ivanmorenogf@hotmail.com>
2024-02-25 16:05:00 +0100

    test: validate git permissions

--- Risk/spec/risk_spec.rb
+++ Risk/spec/risk_spec.rb
@@ -3,29 +3,40 @@
 require 'rspec'
 require_relative '../lib/player'
 require_relative '../lib/risk'
-require_relative '../lib/community'
 
 describe 'Risk' do
-  it 'distributes units to players' do
-    sut = Risk.new
-    doc = Player.new
+  context 'Risk' do
+    it 'distributes units to players' do
+      sut = Risk.new
+      doc = Player.new
 
-    sut.sit(doc)
-    sut.start
+      sut.sit(doc)
+      sut.start
 
-    expect(doc.recruited_units).to be(15)
-  end
+      expect(doc.recruited_units).to be(15)
+    end
 
-  it 'distributes communities to players' do
-    sut = Risk.new
-    player1 = Player.new
-    player2 = Player.new
+    it 'DELETE ME' do
+      sut = Risk.new
+      doc = Player.new
 
-    sut.sit(player1)
-    sut.sit(player2)
-    sut.start
+      sut.sit(doc)
+      sut.start
 
-    expect(player1.owned_communities.length).not_to be(0)
-    expect(player2.owned_communities.length).not_to be(0)
+      expect(doc.owned_communities.length).not_to be(0)
+    end
+
+    it 'distributes communities to players' do
+      sut = Risk.new
+      player1 = Player.new
+      player2 = Player.new
+
+      sut.sit(player1)
+      sut.sit(player2)
+      sut.start
+
+      expect(player1.owned_communities.length).not_to be(0)
+      expect(player2.owned_communities.length).not_to be(0)
+    end
   end
 end
